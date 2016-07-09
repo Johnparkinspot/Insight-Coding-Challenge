aaa952838f4c7b72d9dd380c41b4c73adaeac3c8
Johnparkinspot <johnparkinspot@gmail.com>
2016-07-09 13:38:13 -0700

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <stdio.h> 
+#include "graph.h"
+#include <string> 
+#include <fstream> 
+#include <sstream> 
+#include <memory> 
+#include <time.h>
+#include <cmath>
+#include <iomanip> // for std::precision() std::fixed
+
+int counter = 0; 
+
+// will only store the LATEST median number in the output file. 
+void clear_output_text_file(graphnode * g)
+{
+	std::ofstream ofs; 
+	// the ofstream::trunc option deletes the content before writing anything in 
+	ofs.open("venmo-trans-output.txt", std::ofstream::out | std::ofstream::trunc);  
+//	ofs << g->getLatestMedian(); 
+
+	ofs.close(); 
+}
+
+void writeMedianToOutputFile(graphnode * g)
+{
+	std::ofstream ofs;
+	double median = g->getLatestMedian(); // Now set Precision of median to 3
+	double intpart; 
+	double fractpart = modf(median, &intpart);
+	fractpart = roundf(fractpart * 1000.0) / 1000.0;
+	double medianFinal = intpart + fractpart; 
+	ofs.open("venmo-trans-output.txt", std::ios_base::app); // appends to text file.  
+	ofs << std::fixed << std::setprecision(2) << medianFinal << std::endl; // so every median is in x.xx format . 
+	ofs.close(); 
+}
+
+void parse_string(string s, graphnode *g)
+{
+	// Parses each part of the transaction, actor, target, and timestamp. 
+	string target, actor; 
+	std::istringstream iss(s);
+	for (int i = 0; i < 6; ++i)
+	{
+		string temp; 
+		iss >> temp; 
+		if (i == 1) // parse timestamp 
+		{
+			temp.erase(0, 1); // pop front 
+			temp.erase(temp.size() - 2, 2); 
+			g->store_timestamp(temp); 
+		}
+		if (i == 3) // parse target
+		{
+			temp.erase(0, 1); 
+			temp.erase(temp.size() - 2, 2); 
+			target = temp; 
+		}
+		if (i == 5) // parse actor
+		{ 
+			temp.pop_back(); // remove right bracket 
+			if (temp.size() > 2) // is not ""
+			{
+				temp.erase(0, 1);
+				temp.pop_back();
+				g->add_edge(target, temp); 
+				g->removeEdgesLessThanLatestTimeStamp(); 
+				g->getDegrees(); 
+				g->storeMedian(); 
+				writeMedianToOutputFile(g); 
+			}
+		}	
+	}
+	++counter; 
+}
+
+int main()
+{
+	string line; 
+	graphnode * g = new graphnode(); 
+
+	clear_output_text_file(g);
+	
+	std::ifstream transactions("venmo-trans.txt"); 
+	if (transactions.is_open())
+	{
+		while (getline(transactions,line))
+		{
+			parse_string(line, g); 
+		}
+		transactions.close();
+	}
+	else
+		std::cout << "Unable to open file" << std::endl; 
+
+	std::cout << g->numberOfVerticies() << std::endl; 
+	
+	std::cout << "Latest Median is: ";
+	g->printLatestMedian(); 
+
+
+ 	return 0; 
+}
